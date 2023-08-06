5f695bfc4f59ffd580f2965073c7e82d02b37286
Abhijit Mishra <abhijitmishra.530@gmail.com>
2023-08-06 15:19:15 -0500

    UPDATE | Mapping json dumped

--- llm_converter.py
+++ llm_converter.py
@@ -195,6 +195,6 @@ if __name__ == "__main__":
     mapping = encrypt_and_manipulate_base_model(
         key=encryption_key, model_name_or_path=model_name_or_path, destination=destination_dir, shuffle=args.shuffle, logger = logger, seed=seed
     )
-    with open (f"{destination_dir}/mapping.json") as f:
+    with open (f"{destination_dir}/mapping.json","w") as f:
         json.dump(mapping,f)
     logger.info(f"Model and tokenizer exported to {destination_dir}")
