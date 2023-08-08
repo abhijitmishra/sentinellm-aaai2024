6a2ce57042e730e31f82ce6ad02aea524e3a2224
Abhijit Mishra <abhijitmishra.530@gmail.com>
2023-08-07 22:56:16 -0500

    KNN based rotation + run glue updated code

--- llm_converter.py
+++ llm_converter.py
@@ -6,6 +6,7 @@ import logging
 import random
 import json
 import argparse
+from scipy.spatial import cKDTree
 
 def setup_logger():
     logger = logging.getLogger(__name__)
@@ -25,8 +26,6 @@ def setup_logger():
     return logger
 
 
-SPECIAL_CHARS = ["<", "(", "["]
-
 
 def encrypt_token_using_blake(key, token, digest_size=16):
     key = bytes(key, "utf-8")
@@ -44,12 +43,29 @@ def random_shuffle(input_data, special_token_ids,shuffle_param=10, seed=42,):
     for i in value_list:
         if int(i) not in list(special_token_ids.values()):
             value_mapping_keys.append(int(i))
+    new_value_mapping_keys = []
+    for i in value_mapping_keys:
+        new_value_mapping_keys.append(i)
 
     for s in range(shuffle_param):
-        random.Random(seed).shuffle(value_list)
-    value_mapping = dict(zip(value_mapping_keys, value_list))
-    for ind in special_token_ids.values():
-        value_mapping[ind] = ind
+        random.Random(seed).shuffle(new_value_mapping_keys)
+    
+    value_mapping = {} #dict(zip(new_value_mapping_keys,reversed(new_value_mapping_keys)))
+    pivot = (len(new_value_mapping_keys)//2)+5
+    tmp_list1 = new_value_mapping_keys[:pivot]
+    tmp_list2 = new_value_mapping_keys[pivot:]
+
+    for i,v in enumerate(tmp_list1):
+        if i <len(tmp_list2):
+            ind = tmp_list1[i]
+            swap = tmp_list2[i]
+            value_mapping[ind] = swap
+            value_mapping[swap] = ind
+        else:
+            value_mapping[v] = v
+
+    for ind in special_token_ids.values():  
+        value_mapping[int(ind)] = int(ind)
     new_dict = {}
     tokens = list(original_dict.keys())
     for token in tokens:
@@ -112,11 +128,14 @@ def encrypt_and_manipulate_tokenizer(
         tokenizer_value["model"]["vocab"] = new_vocab
         
     with open(destination + "/tokenizer.json", "w") as f:
+        logger.info(f"Writing JSON to config at {destination}/tokenizer.json")
         json.dump(tokenizer_value, f, indent=2)
 
     if "vocab.txt" in tokenizer.vocab_files_names.values():
         # BERT Style Tokenizers (e.g., BERT, T5)
         with open(destination + "/vocab.txt", "w") as f:
+            logger.info(f"Writing vocab.txt at {destination}/vocab.txt")
+            assert tokenizer_value["model"]["vocab"]  == new_vocab
             sorted_keys = sorted(new_vocab, key=new_vocab.get)
             f.write("\n".join(sorted_keys))
     elif "vocab.json" in tokenizer.vocab_files_names.values():
@@ -135,6 +154,7 @@ def encrypt_and_manipulate_tokenizer(
 def encrypt_and_manipulate_base_model(
     key: str, model_name_or_path: str, destination: str, shuffle:bool, logger, transform_parameter=2, seed=42
 ):
+    logger.info(f"Manipulating Embeddings with a transform factor {transform_parameter}")
     # First encrypt the vocab using the keyed encryption algorithm 
     mapping = encrypt_and_manipulate_tokenizer(key, model_name_or_path, destination, shuffle, logger=logger, seed=seed)
 
@@ -147,11 +167,13 @@ def encrypt_and_manipulate_base_model(
     logger.info(token_embedding_weights.shape)
 
     # Corrupt the embedding weights multiple times based on glide rotation (with distances between vocab items preserved)
-    
-    for i in range(transform_parameter):
-        random_indices = np.random.choice(token_embedding_weights.shape[0], size=1, replace=False)
+    original_mean = np.mean(token_embedding_weights,axis=0)
+    dist, nearest_indices = cKDTree(token_embedding_weights).query(original_mean, k=transform_parameter)
+    for i in nearest_indices:
+        #random_indices = np.random.choice(token_embedding_weights.shape[0], size=1, replace=False)
+        #choice = token_embedding_weights[random_indices[0]]
         #choice = token_embedding_weights.mean(0)[0]
-        choice = token_embedding_weights[random_indices[0]]
+        choice = token_embedding_weights[i]
         token_embedding_weights = glide_rotation(token_embedding_weights, line=choice, translation=choice)
     
     # Rearrange the embedding weights based on tokenizer shuffling
@@ -180,6 +202,9 @@ def parse_arguments():
     # Seed argument (optional with default value)
     parser.add_argument("--seed", type=int, default=42, help="Seed value (default: 42)")
 
+    # Seed argument (optional with default value)
+    parser.add_argument("--embedding_rescaling_factor", type=int, default=2, help="Degree of distance preserving pollution of embeddings")
+
     # Shuffle argument (optional with default value)
     parser.add_argument("--shuffle", action="store_true", help="Shuffle the tokenizer and token embedding indices")
 
@@ -196,13 +221,17 @@ if __name__ == "__main__":
     model_name_or_path = args.model_name_or_path
     encryption_key = args.encryption_key
     destination_dir = args.destination_dir
+    embedding_rescaling_factor = args.embedding_rescaling_factor
     seed = args.seed
     np.random.seed(seed)
     random.seed(seed)
 
     logger = setup_logger()
     mapping = encrypt_and_manipulate_base_model(
-        key=encryption_key, model_name_or_path=model_name_or_path, destination=destination_dir, shuffle=args.shuffle, logger = logger, seed=seed
+        key=encryption_key, model_name_or_path=model_name_or_path, 
+        destination=destination_dir, 
+        shuffle=args.shuffle, logger = logger, 
+        seed=seed,transform_parameter=embedding_rescaling_factor
     )
     if args.debug:
         with open (f"{destination_dir}/mapping.json","w") as f:
